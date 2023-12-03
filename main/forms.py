6867b3d9bb1025456761a5d3ca629ed996924256
kang junemo <103883786+peter0107@users.noreply.github.com>
2023-12-03 13:36:27 +0900

    file 정리

---
+++ main/forms.py
@@ -0,0 +1,57 @@
+#로그인 및 회원가입 처리
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length, Regexp
+from models import Member
+from init import bcrypt
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('이메일', validators=[
+        DataRequired(),
+        Regexp(r'^[a-zA-Z0-9._%+-]+$', message='유효한 이메일 주소를 입력하세요')
+    ])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        validators=[
+            InputRequired(),
+            EqualTo("password", message="비밀번호가 일치하지 않습니다."),
+        ]
+    )
+    nickname = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="닉네임은 3글자 이상, 20글자 이하로 입력해주세요."),
+            Regexp(
+                "^[ㄱ-ㅎ가-힣A-Za-z][ㄱ-ㅎ가-힣A-Za-z0-9_.]*$",
+                0,
+                "닉네임은 한글, 영어, 숫자, '.', '_'만 사용할 수 있습니다. ",
+            ),
+        ]
+    )
+    submit = SubmitField('가입하기')
+    
+    def validate_email(self, email):
+        if Member.query.filter_by(login_id=(email.data + "@g.skku.edu")).first():
+            raise ValidationError("이미 등록된 이메일입니다!")
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('ID', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('로그인')
+    
+    def validate_email(self, email):
+        member = Member.query.filter_by(login_id=email.data).first()
+        
+        if member is None:
+            raise ValidationError("등록되지 않은 이메일 주소입니다")
+    
+    def validate_password(self, password):
+        member = Member.query.filter_by(login_id=self.email.data).first()
+        
+        if member is not None:
+            if not bcrypt.check_password_hash(member.password, self.password.data):
+                raise ValidationError("비밀번호가 일치하지 않습니다.")
---
+++ main/init.py
@@ -0,0 +1,51 @@
+#Flask 시작 전 준비사항
+
+from flask import Flask
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+from config import MAIL_SENDER, MAIL_PASSWORD, DATABASE_URI
+
+
+# Flask-Login 초기화
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+db = SQLAlchemy()
+mail = Mail()
+bcrypt = Bcrypt()
+cors = CORS()
+
+# Flask 애플리케이션에 스케줄러 추가
+scheduler = BackgroundScheduler()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'default_secret_key')
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Flask-Mail 설정
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = MAIL_SENDER
+    app.config['MAIL_PASSWORD'] = MAIL_PASSWORD
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_DEFAULT_SENDER'] = MAIL_SENDER
+    
+    login_manager.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
+    bcrypt.init_app(app)
+    cors.init_app(app)
+    
+    return app
