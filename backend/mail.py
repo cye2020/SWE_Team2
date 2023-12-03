e6eff523dab14cdb22b64a02674ca9c6dd34665f
kang junemo <103883786+peter0107@users.noreply.github.com>
2023-12-03 13:41:41 +0900

    Delete backend directory

--- backend/mail.py
+++
@@ -1,9 +0,0 @@
-from flask_mail import Message
-from flask import url_for
-from init import mail
-
-def send_verification_email(email, token):
-    subject = '이메일 인증'
-    body = f'인증 링크: {url_for("verify_email", token=token, _external=True)}'
-    message = Message(subject, recipients=[email], body=body)
-    mail.send(message)
