b2afdc0df417c7129d6742ad5761d1bf79375d5c
cye2020 <ariaplay101@gmail.com>
2023-12-03 17:38:38 +0900

    Delete Config

--- main/config.py
+++
@@ -1,8 +0,0 @@
-#중요정보들 저장
-
-# Mail 설정
-MAIL_SENDER = 'seongdaeuijib@gmail.com'
-MAIL_PASSWORD = 'rzor wmwd vaiz bidq'
-
-# AWS RDS 설정
-DATABASE_URI = 'mysql+pymysql://root:peter0107@database-1.cc0lokhfxaeb.us-east-2.rds.amazonaws.com/software_database'
